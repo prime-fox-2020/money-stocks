2d6798b77a6e2039c69c9a06e55fb64c76f1910c
Agustinus Adam <agustinusadamw@gmail.com>
2020-04-04 13:43:07 +0700

    money-stocks done

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-//Diberikan program untuk mengubah
 const moneyStocks = {
   100000: 1,
   50000: 2,
@@ -8,93 +7,83 @@ const moneyStocks = {
   1000: 10,
   500: 5
 };
+// console.log('moneyStocks: ', moneyStocks);
 
-//MULAI BAGIAN CODE UNTUK DEBUG
-let uang = [];
+function getMoneyChange(num) {
+  //mengubah obj menjadi arr
+  const stock = Object.entries(moneyStocks)
 
-for (const key in moneyStocks) {
-  uang.push(+key);
-}
-for (let i = 0; i < uang.length - 1; i++) {
-  for (let j = i + 1; j < uang.length; j++) {
-    if (uang[i] < uang[j]) {
-      let swap = uang[i];
-      uang[i] = uang[j];
-      uang[j] = swap;
-    }
+  //ketersediaan uang
+  let totalUang = 0
+  for (let i = 0; i < stock.length; i++) {
+      totalUang += stock[i][0] * stock[i][1];
   }
-}
-console.log(uang);
+  console.log('totalUang: ', totalUang);
 
-function getMoneyChange(mauDiTukar) {
-  let obj = {};
-  for (let i = 0; i < uang.length; i++) {
-    while (moneyStocks[uang[i]] != 0 && mauDiTukar - uang[i] >= 0) {
-      if (!obj[uang[i]]) {
-        obj[uang[i]] = 0;
+  //syarat mencari kembalian di jalankan
+  if (totalUang < num) {
+      return console.log(`Maaf uang kembalian tidak cukup`)
+  } else {
+      let kembalian = {}
+      for (let k = stock.length - 1; k >= 0; k--) {
+          while (num >= stock[k][0] && stock[k][1] > 0) {
+              if (stock[k][1] > 0) {
+                  stock[k][1]--
+                  num -= stock[k][0]
+                  moneyStocks[stock[k][0]]--
+                  if (kembalian[stock[k][0]] == undefined) {
+                      kembalian[stock[k][0]] = 0
+                  }
+                  kembalian[stock[k][0]]++
+              }
+          }
       }
-      moneyStocks[uang[i]] -= 1;
-      mauDiTukar -= uang[i];
-      obj[uang[i]] += 1;
-    }
-  }
 
-  // console.log(obj);
-
-  if (mauDiTukar != 0) {
-    for (const key in obj) {
-      let sample = +key;
-      moneyStocks.sample += obj[key];
-    }
-    console.log('Maaf uang kembalian tidak cukup');
+      kembalian = Object.entries(kembalian)
+      // console.log('kembalian: ', kembalian);
+      for (let i = kembalian.length - 1; i >= 0; i--) {
+          console.log(`${kembalian[i][0]} ${kembalian[i][1]} lembar`);
+      }
   }
 }
-//AKHIR DARI BAGIAN DEBUG
 
-console.log('CASE 1:\n----------------');
 getMoneyChange(75000);
 /*
-    50000 1 lembar
-    20000 1 lembar
-    5000 1 lembar
+50000 1 lembar
+20000 1 lembar
+5000 1 lembar
 */
-console.log('');
-console.log('CASE 2:\n----------------');
+console.log(`\n`);
 getMoneyChange(190000);
 /*
-    100000 1 lembar
-    50000 1 lembar
-    20000 2 lembar
+100000 1 lembar
+50000 1 lembar
+20000 2 lembar
 */
-console.log('');
-console.log('CASE 3:\n----------------');
 getMoneyChange(190000);
 /*
-    Maaf uang kembalian tidak cukup
+Maaf uang kembalian tidak cukup
 */
-console.log('');
-console.log('CASE 4:\n----------------');
+console.log(`\n`);
 getMoneyChange(100000);
 /*
-    20000 1 lembar
-    10000 5 lembar
-    5000 4 lembar
-    1000 10 lembar
+20000 1 lembar
+10000 5 lembar
+5000 4 lembar
+1000 10 lembar
 */
-console.log('');
-console.log('CASE 5:\n----------------');
+console.log(`\n`);
 getMoneyChange(400);
-    /*
-    Maaf uang kembalian tidak cukup
+/*
+Maaf uang kembalian tidak cukup
 */
-    // console.log('');
-    // console.log(moneyStocks);
+console.log(moneyStocks);
 /*
 { '500': 5,
-    '1000': 0,
-    '5000': 0,
-    '10000': 0,
-    '20000': 0,
-    '50000': 0,
-    '100000': 0 }
+'1000': 0,
+'5000': 0,
+'10000': 0,
+'20000': 0,
+'50000': 0,
+'100000': 0 }
 */
